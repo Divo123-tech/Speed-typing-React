d68787f8e43fe50b3ced29df0b8e02df7ec02fb2
Divo123 <topherharsh@gmail.com>
2023-05-15 06:29:19 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>time remaining: 5</h4>
+      <button>START</button>
+      <h1>Word Count: 5</h1>
+    </div>
+  )
+}
+
+export default App
